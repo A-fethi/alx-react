33407c98a4a681ed19d896eb6125f97862511c24
A-fethi <fethiabderrahmane1@gmail.com>
2024-01-29 03:05:28 -0800

    javascript file

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,10 @@
+const $ = require( "jquery" );
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id='count'></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+function updateCounter() {
+}
