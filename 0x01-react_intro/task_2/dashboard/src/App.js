b6bd12655eb5c911e763a3df98a72a38d42f6313
FETHI Abderrahmane <fethiabderrahmane1@gmail.com>
2024-01-30 18:49:57 +0100

    Modifying the Notifications

--- 0x01-react_intro/task_2/dashboard/src/App.js
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -11,10 +11,10 @@ function App() {
       </div>
       <div className="App-body">
           <p>Login to access the full dashboard</p>
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email"/>
-          <label htmlFor="password">Password</label>
-          <input type="password" id="password"/>
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" style={{ marginLeft: '10px' }}/>
+          <label htmlFor="password" style={{ marginLeft: '10px' }}>Password:</label>
+          <input type="password" id="password" style={{ marginLeft: '10px' }}/>
           <button>OK</button>
       </div>
       <div className="App-footer">
