5100aab11591dfd249246f1673510db8d09bd39a
FETHI Abderrahmane <fethiabderrahmane1@gmail.com>
2024-01-31 14:52:29 +0100

    Deploying to a Github page

---
+++ 0x01-react_intro/task_4/dashboard/src/App.test.js
@@ -0,0 +1,24 @@
+import { render } from '@testing-library/react';
+import App from './App';
+import { shallow } from 'enzyme';
+
+test('renders without crashing', () => {
+  render(<App />);
+});
+
+describe('<App />', () => {
+  it('renders a div with the class App-header', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('div.App-header').exists()).toBe(true);
+  });
+
+  it('renders a div with the class App-body', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('div.App-body').exists()).toBe(true);
+  });
+
+  it('renders a div with the class App-footer', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('div.App-footer').exists()).toBe(true);
+  });
+})
