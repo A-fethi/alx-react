7d3c5d65b87449812cb52310beff1b537499910a
FETHI Abderrahmane <fethiabderrahmane1@gmail.com>
2024-01-31 19:05:19 +0100

    Reorganizing the files and making tests

---
+++ 0x01-react_intro/task_5/dashboard/src/App/App.js
@@ -0,0 +1,28 @@
+import logo from '../assets/Holberton-logo.jpg';
+import { getFullYear, getFooterCopy } from '../utils/utils';
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <div className="App-header">
+        <img src={logo} alt="logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+          <p>Login to access the full dashboard</p>
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" style={{ marginLeft: '10px' }}/>
+          <label htmlFor="password" style={{ marginLeft: '10px' }}>Password:</label>
+          <input type="password" id="password" style={{ marginLeft: '10px' }}/>
+          <button>OK</button>
+      </div>
+      <div className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy()}</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
