be9c2cce8f55ea8a1f53bf525e323699ea701942
FETHI Abderrahmane <fethiabderrahmane1@gmail.com>
2024-02-08 11:17:36 +0100

    Declaring a pure component

---
+++ 0x03-React_component/task_5/dashboard/src/Notifications/NotificationItem.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import NotificationItem from './NotificationItem';
+
+describe('NotificationItem component', () => {
+    it('should render without crashing', () => {
+        shallow(<NotificationItem />);
+    });
+
+    it('should render correct html from type and value props', () => {
+        const wrapper = shallow(<NotificationItem type="default" value="test" />);
+        expect(wrapper.html()).toEqual('<li data-notification-type="default">test</li>');
+    });
+
+    it('should render correct html from html="<u>test</u>" props', () => {
+        const wrapper = shallow(<NotificationItem html="<u>test</u>" />);
+        expect(wrapper.html()).toEqual('<li data-notification-type=\"default\"><u>test</u></li>');
+    });
+
+    it('should render the right html', () => {
+        const wrapper = shallow(<NotificationItem />);
+        expect(wrapper.find('li').at(0).html()).toEqual('<li data-notification-type=\"default\"></li>');
+    });
+
+    it('calls markAsRead with the right id argument on click', () => {
+        const mockMarkAsRead = jest.fn();
+        const wrapper = shallow(<NotificationItem markAsRead={mockMarkAsRead} id={1} />);
+        wrapper.find('li').simulate('click');
+        expect(mockMarkAsRead).toHaveBeenCalledWith(1);
+    });
+});
