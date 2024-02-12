4d6e8b617df45011c5668daa99226f82ba7bb8b8
FETHI Abderrahmane <fethiabderrahmane1@gmail.com>
2024-02-12 17:27:39 +0100

    installing Aphrodite & modifying components

---
+++ 0x04-React_inline_styling/task_1/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './Notifications.css';
+import PropTypes from 'prop-types';
+
+export default class NotificationItem extends React.PureComponent {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        const { type, value, html, markAsRead, id } = this.props;
+        return (
+            <li
+                data-notification-type={type}
+                dangerouslySetInnerHTML={html && { __html: html }}
+                onClick={() => markAsRead(id)}
+            >
+                {value}
+            </li>
+        );
+    }
+}
+
+NotificationItem.proptypes = {
+    __html: PropTypes.shape({
+        html: PropTypes.string,
+    }),
+    type: PropTypes.string.isRequired,
+    value: PropTypes.string,
+    markAsRead: PropTypes.func.isRequired,
+    id: PropTypes.number.isRequired,
+};
+
+NotificationItem.defaultProps = {
+    type: "default",
+};
