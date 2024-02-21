de640ef14b5c3cb042f192ff5791779cbb2e163e
A-fethi <fethiabderrahmane1@gmail.com>
2024-02-21 07:02:16 -0800

    Merge & concat

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,10 +1,10 @@
 import { List, Map } from 'immutable';
 
-export default function concatElements(page1, page2) {
+export function concatElements (page1, page2) {
   return List(page1.concat(page2));
-};
+}
 
-export default function mergeElements(page1, page2) {
+export function mergeElements (page1, page2) {
   const map1 = Map(page1);
   const map2 = Map(page2);
   const mergedMap = map1.merge(map2);
