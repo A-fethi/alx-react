a81d364dc9ef34b6c2b97dd4c66413bad2ac644c
FETHI Abderrahmane <fethiabderrahmane1@gmail.com>
2024-03-08 14:47:23 +0100

    Combining store

--- 0x09-react_redux_connectors_and_providers/task_4/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_4/dashboard/src/index.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from "./App/App";
-import uiReducer, { initialState } from './reducers/uiReducer'
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import { thunk } from 'redux-thunk';
-import { Map } from 'immutable';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer, { initialRootState } from './reducers/rootReducer';
 
 const root = createRoot(document.getElementById("root"));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
-    uiReducer,
-    Map(initialState),
-    composeWithDevTools(
+    combineReducers(rootReducer),
+    initialRootState,
+    composeEnhancers(
         applyMiddleware(thunk)
     )
 );
